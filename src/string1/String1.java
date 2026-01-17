8d09592e7798c7fade5e1f03467cb5f48050b108
jibek abdyrazakova <jibekabdyrazakova@jibeks-MacBook-Air.local>
2026-01-17 18:29:31 +0600

    helloName

---
+++ src/string1/String1.java
@@ -0,0 +1,10 @@
+package string1;
+
+public class String1 {
+    public static void main(String[] args) {
+
+    }
+    public String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}
